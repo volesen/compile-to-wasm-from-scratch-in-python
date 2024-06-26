813709f7128ba78d9eccb3634f10c06853a787b2
volesen <vincolesen@gmail.com>
2024-06-26 17:05:18 +0200

    Use PEG parser generator for parsing

--- src/compile_to_wasm_from_scratch/__init__.py
+++ src/compile_to_wasm_from_scratch/__init__.py
@@ -1,29 +1,15 @@
 import argparse
-from .scanner import scanner
-from .parser import CTWFSParser, ParserError
+from .parser import parse
 from .compiler import compile_prog, serialize_sexpr
 
 
 def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=argparse.FileType("r"))
+
     args = parser.parse_args()
 
-    tokens, remainder = scanner.scan(args.input.read())
-
-    if remainder:
-        print(f"Unrecognized input: {remainder}")
-        return 1
-
-    parser = CTWFSParser(tokens)
-
-    try:
-        program = parser.parse()
-    except ParserError as e:
-        print(e)
-        print(f"{parser.tokens[parser.current:]=}")
-        raise
-        return 1
+    program = parse(args.input)
 
     wat = compile_prog(program)
 
